80802104daa7fd64a9087194e89b8f1aad8b9c7d
H51M14\Student <Student@gmail.com>
2025-08-28 08:51:54 +0700

    bai 7

---
+++ src/bai7.ts
@@ -0,0 +1,13 @@
+function firstPromise(time: number, name: string): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(`${name} finish in ${time}`);
+      }, time);
+    });
+  }
+  
+const task1 = firstPromise(3000, "Task 1");
+const task2 = firstPromise(1000, "Task 2");
+const task3 = firstPromise(2000, "Task 3");
+ 
+Promise.race([task1,task2 ,task3]).then((result)=> console.log("Fastest task",result))
