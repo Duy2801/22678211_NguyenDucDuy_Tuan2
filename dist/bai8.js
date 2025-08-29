af7bf7081dbd82740f1e13028b9e6ce482967aa3
Duy2801 <nguyenducduy544645@gmail.com>
2025-08-29 17:59:34 +0700

    bai 8

---
+++ dist/bai8.js
@@ -0,0 +1,18 @@
+"use strict";
+Promise.resolve(2)
+    .then((value) => {
+    console.log(`Starting with: ${value}`);
+    return value * value;
+})
+    .then((value) => {
+    console.log(`After square: ${value}`);
+    return value * 2;
+})
+    .then((value) => {
+    console.log(`After double: ${value}`);
+    return value + 5;
+})
+    .then((value) => {
+    console.log(`Final result: ${value}`);
+    return value;
+});
